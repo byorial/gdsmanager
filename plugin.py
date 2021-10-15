c4ef000fba87eb8205f1cd022e8c2aaf42c0837a
byorial <orial@orial.kr>
2021-10-15 17:19:49 +0900

    v0.1.2.9

--- plugin.py
+++ plugin.py
@@ -37,7 +37,7 @@ class P(object):
         },
     }
     plugin_info = {
-        'version' : '0.1.2.8',
+        'version' : '0.1.2.9',
         'name' : package_name,
         'category_name' : 'service',
         'icon' : '',
